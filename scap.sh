b4359f8f37e12918e053232ada00734f11c42b1f
Andrew Cowan <andrew.cowan@persephone.biz>
2022-02-18 20:06:30 +0100

    Added sysctl hardening

---
+++ scap.sh
@@ -0,0 +1,4 @@
+cd /usr/local
+wget https://github.com/OpenSCAP/scap-security-guide/releases/download/v0.1.52/scap-security-guide-0.1.52.zip
+unzip scap-security-guide-0.1.52.zip
+cd /usr/local/scap-security-guide-0.1.52
